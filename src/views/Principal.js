0dbd8c10b09a65088c56c607f22e96d24e96b8be
Kevin Nicolas Ceron Catumba <knceron4@misena.edu.co>
2022-08-19 09:25:22 -0500

    SE REALIZO INVESTICACION DE REDIRECCIONES

--- src/views/Principal.js
+++ src/views/Principal.js
@@ -7,14 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          BIENVENIDO A NICOKCC BLOG
+          PRINCIPAL
         </p>
         <a
           className="App-link"
           href="https://instagram.com/kevin.nicoolas"
           target="_blank"
           rel="noopener noreferrer"
-        >LINK A INCIO</a>
+        >:)</a>
       </header>
     </div>
   );
